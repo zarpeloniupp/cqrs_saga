81f3a320b0b9162dbb57e9598f0c3383cfb880bd
Michel Zarpelon <michel.zarpelon.iupp@zup.com.br>
2022-12-24 21:40:58 -0300

    product, api e eureka

---
+++ product-server/src/main/java/com/zarpelon/estore/productserver/ProductServerApplication.java
@@ -0,0 +1,15 @@
+package com.zarpelon.estore.productserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ProductServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServerApplication.class, args);
+	}
+
+}
---
+++ product-server/src/main/java/com/zarpelon/estore/productserver/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package com.zarpelon.estore.productserver.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    public String get() {
+        return "GET Products";
+    }
+}
